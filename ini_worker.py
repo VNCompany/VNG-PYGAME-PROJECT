eefdeac1b1ca76d031755c42aa2660aee6758738
VNCompany <vneznanow@yandex.ru>
2020-01-15 00:07:52 +0300

    #19. New class - INI

--- ini_worker.py
+++ ini_worker.py
@@ -1,11 +1,10 @@
 import re
 
 
-# Function by Victor Neznanov
-def ini_parse(filename: str):
-    with open(filename, mode="r", encoding="utf-8") as f:
-        lines = f.readlines()
-        ini = {}
+# Class by Victor Neznanov
+class INI:
+    def __init__(self, lines: list):
+        self.data = {}
 
         section = "Default"
         for line in lines:
@@ -16,8 +15,33 @@ def ini_parse(filename: str):
             if line != "":
                 search = re.search(r"(.+?)=(.*)", line)
                 if search is not None:
-                    if section in ini.keys():
-                        ini[section].append((search.group(1), search.group(2)))
+                    if section in self.data.keys():
+                        self.data[section].append((search.group(1), search.group(2)))
                     else:
-                        ini[section] = [(search.group(1), search.group(2))]
-        return ini
+                        self.data[section] = [(search.group(1), search.group(2))]
+
+    @staticmethod
+    def ini_parse(filename: str):
+        with open(filename, mode="r", encoding="utf-8") as f:
+            lines = f.readlines()
+            try:
+                return INI(lines)
+            except Exception as e:
+                print("Load file error:", str(e))
+
+    @staticmethod
+    def ini_parse_text(text: str):
+        INI(text.split("\n"))
+
+    def get(self, section: str, item: str):
+        if section in self.data.keys():
+            for key, value in self.data[section]:
+                if key == item:
+                    return value
+        return None
+
+    def section_exists(self, section: str):
+        return section in self.data.keys()
+
+    def get_sections(self):
+        return list(self.data.keys())
